b75eec2e5ebc4fcb808292051ba3e2f5b3ee1dad
Hidekazu Kobayashi <kobahide789@gmail.com>
2015-01-18 15:29:46 +0900

    create frame layout

--- gulpfile.js
+++ gulpfile.js
@@ -13,28 +13,33 @@ var gulp = require('gulp'),
     browserify = require('browserify');
 
 var SRC = {
-  LESS: './src/less/**/*.less',
+  LESS: './src/less/*.less',
   HTML: './src/*.html',
-  JS: './src/js/index.js'
+  JS: './src/js/index.js',
+  FONT: './src/fonts/*'
 };
 
 var DIST = {
   CSS: './dist/css/',
   HTML: './dist/',
-  JS: './dist/js/'
+  JS: './dist/js/',
+  FONT: './dist/fonts/'
 };
 
 gulp.task('less', function () {
   gulp.src(SRC.LESS)
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(less())
+    .pipe(gulp.dest(DIST.CSS));
 });
 
 gulp.task('html', function () {
   gulp.src(SRC.HTML)
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(DIST.HTML));
+});
+
+gulp.task('font', function () {
+  gulp.src(SRC.FONT)
+    .pipe(gulp.dest(DIST.FONT));
 });
 
 var bundler = browserify({
@@ -49,19 +54,21 @@ gulp.task('js', function () {
     .pipe(gulp.dest(DIST.JS));
 });
 
-gulp.task('default', ['html', 'less', 'js']);
+gulp.task('default', ['html', 'less', 'fonts', 'js']);
 
 gulp.task('watch', function () {
   gulp.src(SRC.HTML)
     .pipe(watch(SRC.HTML))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(DIST.HTML));
 
   gulp.src(SRC.LESS)
     .pipe(watch(SRC.LESS))
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(less())
+    .pipe(gulp.dest(DIST.CSS));
+
+  gulp.src(SRC.FONT)
+    .pipe(watch(SRC.FONT))
+    .pipe(gulp.dest(DIST.FONT));
 
   var wbundler = watchify(bundler);
   function bundle () {
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
+'use strict';
 
+global.jQuery = require('jquery');
+require('bootstrap');
+
+var hg = require('mercury');
+var h = require('mercury').h;
+
+function App() {
+  this.state = hg.state({
+    value: hg.value(0),
+    channels: {
+      
+    }
+  });
+}
+
+App.prototype.render = function render(state) {
+  return h('h1', [
+    'ダッシュのボード'
+  ]);
+};
+
+var app = new App();
+
+hg.app(document.getElementById('main'), app.state, app.render.bind(app));
